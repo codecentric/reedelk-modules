481d70b9175fc0a77633b36f2eb277fad82943d1
lorenzo <lorenzo@lorenzo.info.com>
2019-02-25 17:49:10 +0100

    Feature #84: rest-module: development (Step 2) add functionality to perform file upload (multipart/www-form-url-encoded/chunked)

--- rest-module/src/main/java/com/esb/foonnel/rest/SetStatus.java
+++ rest-module/src/main/java/com/esb/foonnel/rest/SetStatus.java
@@ -2,6 +2,7 @@ package com.esb.foonnel.rest;
 
 import com.esb.foonnel.api.Message;
 import com.esb.foonnel.api.Processor;
+import com.esb.foonnel.rest.http.OutboundProperty;
 import org.osgi.service.component.annotations.Component;
 
 import static org.osgi.service.component.annotations.ServiceScope.PROTOTYPE;
@@ -13,7 +14,7 @@ public class SetStatus implements Processor {
 
     @Override
     public Message apply(Message message) {
-        message.setHttpStatus(status);
+        OutboundProperty.STATUS.set(message, status);
         return message;
     }
 
