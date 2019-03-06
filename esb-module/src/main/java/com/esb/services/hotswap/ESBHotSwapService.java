bb75e8442ea57b12a4cdcf2e151c3b1f50192332
lorenzo <lorenzo@lorenzo.info.com>
2019-03-06 16:30:00 +0100

    Feature #4: (Hotswap): Tests: Allow to deploy the json of the flows without building the jar.

--- esb-module/src/main/java/com/esb/services/hotswap/ESBHotSwapService.java
+++ esb-module/src/main/java/com/esb/services/hotswap/ESBHotSwapService.java
@@ -3,12 +3,16 @@ package com.esb.services.hotswap;
 import com.esb.internal.api.HotSwapService;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
 import static com.esb.commons.Preconditions.checkIsPresentAndGetOrThrow;
 
 public class ESBHotSwapService implements HotSwapService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ESBHotSwapService.class);
 
     private final BundleContext context;
     private final HotSwapListener listener;
@@ -24,6 +28,8 @@ public class ESBHotSwapService implements HotSwapService {
         Bundle bundleAtPath = checkIsPresentAndGetOrThrow(optionalBundle, "Hot Swap failed: could not find registered bundle in target file path=%s", modulePath);
 
         listener.hotSwap(bundleAtPath.getBundleId(), resourcesRootDirectory);
+
+        logger.debug("Module [{}] reloaded", bundleAtPath.getSymbolicName());
         return bundleAtPath.getBundleId();
     }
 
