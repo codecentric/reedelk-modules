c972240632acf93efabefb778e0f7958acbaed28
lorenzo <lorenzo@lorenzo.info.com>
2019-03-18 14:22:55 +0100

    Feature #28: Console admin REST /hotswap API should return 404 not found when /hostwap of a module not installed + Added configurable bind port for dev-admin-console module.

--- admin-console-dev-module/src/main/resources/console/js/deployment-controller.js
+++ admin-console-dev-module/src/main/resources/console/js/deployment-controller.js
@@ -1,11 +1,11 @@
 function listModules() {
-    $.get("/module", function (data) {
+    $.get("/api/module", function (data) {
         $('#deployed-modules').bootstrapTable('load', data.modules);
     });
 }
 
 function updateModule(name, moduleFilePath) {
-    $.post("/module", JSON.stringify({moduleFilePath: moduleFilePath}), function () {
+    $.post("/api/module", JSON.stringify({moduleFilePath: moduleFilePath}), function () {
         toastr.success('Successfully updated module "' + name + '"');
         setTimeout(listModules, 1000);
     }, "json").fail(function (error) {
