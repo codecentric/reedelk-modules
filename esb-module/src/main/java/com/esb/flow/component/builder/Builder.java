2f0ce10701bb4cad0de0f761cd02e6c45308ef2a
lorenzo <lorenzo@lorenzo.info.com>
2019-05-03 09:53:01 +0200

    Refactoring.

--- esb-module/src/main/java/com/esb/flow/component/builder/Builder.java
+++ esb-module/src/main/java/com/esb/flow/component/builder/Builder.java
@@ -3,6 +3,7 @@ package com.esb.flow.component.builder;
 import com.esb.flow.ExecutionNode;
 import org.json.JSONObject;
 
+// TODO: This should be named deserializer,  look at the plugin!!!
 public interface Builder {
 
     ExecutionNode build(ExecutionNode parent, JSONObject componentDefinition);
