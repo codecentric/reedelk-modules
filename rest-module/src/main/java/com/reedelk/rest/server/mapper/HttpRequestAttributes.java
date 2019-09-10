94458503badd0542a5287a61198659a27383a67b
lorenzo <lorenzo@lorenzo.info.com>
2019-09-10 14:08:13 +0200

    code

--- rest-module/src/main/java/com/reedelk/rest/server/mapper/HttpRequestAttributes.java
+++ rest-module/src/main/java/com/reedelk/rest/server/mapper/HttpRequestAttributes.java
@@ -3,6 +3,7 @@ package com.reedelk.rest.server.mapper;
 import com.reedelk.runtime.api.message.AbstractMessageAttributes;
 
 public class HttpRequestAttributes extends AbstractMessageAttributes {
+
     @Override
     public String toString() {
         return HttpRequestAttributes.class.getSimpleName() + super.toString();
