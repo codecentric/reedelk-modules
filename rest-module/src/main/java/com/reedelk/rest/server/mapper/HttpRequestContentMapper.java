2789e282dac697c8f38d5d55c5ac9b4b65be720f
lorenzo <lorenzo@lorenzo.info.com>
2019-09-10 13:40:09 +0200

    code

--- rest-module/src/main/java/com/reedelk/rest/server/mapper/HttpRequestContentMapper.java
+++ rest-module/src/main/java/com/reedelk/rest/server/mapper/HttpRequestContentMapper.java
@@ -3,7 +3,8 @@ package com.reedelk.rest.server.mapper;
 import com.reedelk.runtime.api.commons.JavaType;
 import com.reedelk.runtime.api.message.type.*;
 import io.netty.buffer.ByteBuf;
-import io.netty.util.IllegalReferenceCountException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.SynchronousSink;
 
@@ -13,41 +14,43 @@ import java.util.function.BiConsumer;
 
 class HttpRequestContentMapper {
 
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestContentMapper.class);
+
     /**
      * Given an http request, it finds the most suitable TypedContent for the request.
      * For example, it checks the mime type of the request and it converts it a String
      * if it is a text based mime type, otherwise it keeps as bytes.
      */
     static TypedContent map(HttpRequestWrapper request) {
-        // Map the request content and forward it to
-        // a sink which maps it to a byte buffer.
-        Flux<byte[]> byteArrayStream = request
-                .data()
-                .retain()
-                .handle(asByteArrayStream());
+        // The content stream is fed into a byte sink
+        Flux<byte[]> byteArrayStream =
+                // we retain and we release it when we read the bytes in the sink below.
+                request.data().retain().handle(asByteArrayStream());
 
         MimeType mimeType = request.mimeType();
 
-        Class<?> javaType = JavaType.from(mimeType);
-        if (javaType == String.class) {
-            // If it  is a String, then we check the charset if present
-            // in the mime type to be used for the string conversion.
-            Optional<Charset> charset = mimeType.getCharset();
-
-            // Map each byte array of the stream to a string
-            Flux<String> streamAsString = byteArrayStream.map(bytes -> {
-                Charset conversionCharset = charset.orElseGet(Charset::defaultCharset);
-                return new String(bytes, conversionCharset);
-            });
-
-            // The TypedContent is String stream.
-            return new StringStreamContent(streamAsString, mimeType);
-
+        if (String.class == JavaType.from(mimeType)) {
+            return mapStringJavaType(byteArrayStream, mimeType);
         } else {
             // Generic byte array stream.
             Type type = new Type(mimeType, byte[].class);
             return new ByteArrayStreamContent(byteArrayStream, type);
         }
+    }
+
+    private static TypedContent mapStringJavaType(Flux<byte[]> byteArrayStream, MimeType mimeType) {
+        // If it  is a String, then we check the charset if present
+        // in the mime type to be used for the string conversion.
+        Optional<Charset> charset = mimeType.getCharset();
+
+        // Map each byte array of the stream to a string
+        Flux<String> streamAsString = byteArrayStream.map(bytes -> {
+            Charset conversionCharset = charset.orElseGet(Charset::defaultCharset);
+            return new String(bytes, conversionCharset);
+        });
+
+        // The TypedContent is String stream.
+        return new StringStreamContent(streamAsString, mimeType);
     }
 
     private static BiConsumer<ByteBuf, SynchronousSink<byte[]>> asByteArrayStream() {
@@ -56,8 +59,11 @@ class HttpRequestContentMapper {
                 byte[] bytes = new byte[byteBuffer.readableBytes()];
                 byteBuffer.readBytes(bytes);
                 sink.next(bytes);
+                // Each stream byte buffer is reference counted,
+                // therefore we must release it after reading its bytes.
                 byteBuffer.release();
-            } catch (IllegalReferenceCountException e) {
+            } catch (Exception e) {
+                logger.error("Error while feeding input sink", e);
                 sink.complete();
             }
         };
