bc2ca0df99015e7e49f645f38f2f00f90faac84e
lorenzo <lorenzo@lorenzo.info.com>
2019-10-04 18:17:15 +0200

    code

--- esb-module/src/main/java/com/reedelk/esb/commons/FunctionName.java
+++ esb-module/src/main/java/com/reedelk/esb/commons/FunctionName.java
@@ -4,6 +4,9 @@ public class FunctionName {
 
     private static final String FUNCTION_NAME_TEMPLATE = "fun_%s";
 
+    private FunctionName() {
+    }
+
     public static String from(String uuid) {
         return String.format(FUNCTION_NAME_TEMPLATE, uuid);
     }
